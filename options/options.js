d469b1e51ffa2f05bd0231e31d8052dd6519df43
Crowdbotics <engineering@crowdbotics.com>
2021-09-10 15:24:51 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "tetap_1Identifier",
+  url: "https://tetap_1Identifier.botics.co",
+  api: "https://tetap_1Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
